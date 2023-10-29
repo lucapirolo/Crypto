e5ee12ee07b877010b47d62572811a1d680c6cc8
Luca Pirolo <66700259+lucapirolo@users.noreply.github.com>
2023-10-29 11:00:10 +0000

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,20 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Luca Pirolo on 29/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
