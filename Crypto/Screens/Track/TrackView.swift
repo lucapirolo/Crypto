dd3ebeced0f50a6698614fde84f5462788c6d231
Luca Pirolo <66700259+lucapirolo@users.noreply.github.com>
2023-11-01 10:18:44 +0000

    Refactor And Document TrackvVew

--- Crypto/Screens/Track/TrackView.swift
+++ Crypto/Screens/Track/TrackView.swift
@@ -8,85 +8,110 @@
 import SwiftUI
 import Charts
 
+/// A view for tracking cryptocurrency market data.
 struct TrackView: View {
+    
+    // MARK: - Properties
+    
     @ObservedObject var viewModel: TrackViewModel
     @Environment(\.colorScheme) private var colorScheme
     @State private var searchText = ""
     
-
-    
+    /// Computes the cryptocurrencies to be displayed.
     var cryptos: Cryptos {
-        return viewModel.cryptos ?? viewModel.cachedCryptos.map { $0.toModel() }
+        viewModel.cryptos ?? viewModel.cachedCryptos.map { $0.toModel() }
     }
-
+    
+    /// Filters the cryptocurrencies based on the search text.
     var filteredCryptos: Cryptos {
         guard !searchText.isEmpty else {
             return cryptos
         }
-        
-        return cryptos.filter { crypto in
-            crypto.name.lowercased().contains(searchText.lowercased())
-        }
+        return cryptos.filter { $0.name.lowercased().contains(searchText.lowercased()) }
     }
-
+    
+    // MARK: - Initialization
+    
     init(viewModel: TrackViewModel) {
-           self.viewModel = viewModel
-           UINavigationBar.configureCryptoNavBarAppearance()
-       }
+        self.viewModel = viewModel
+        UINavigationBar.configureCryptoNavBarAppearance()
+    }
+    
+    // MARK: - Body
     
     var body: some View {
         NavigationView {
-            Group {
-                if viewModel.isLoading && filteredCryptos.isEmpty {
-                    ProgressView()
-                } else if !filteredCryptos.isEmpty {
-                    cryptoScrollView
-                } else {
-                    Text("Nothing To Show!")
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                }
+            ZStack {
+                backgroundView
+                contentGroup
+                    .searchable(text: $searchText, prompt: "Search Coins")
+                    .navigationBarTitleDisplayMode(.inline)
+                    .navigationTitle("Track Coins")
+                    .onAppear {
+                        viewModel.loadMarketData()
+                    }
+                    .showAlert(alert: $viewModel.alert)
             }
-            .adaptiveBackgroundColor() // Apply the background to the whole Group
-            .searchable(text: $searchText, prompt: "Search Coins")
-            .navigationBarTitleDisplayMode(.inline)
-            .navigationTitle("Track Coins")
-            .onAppear {
-                viewModel.loadMarketData()
-            }
-            .showAlert(alert: $viewModel.alert)
         }
     }
     
+    // MARK: - Private Views
+    
+    /// The background view for the track view.
+    private var backgroundView: some View {
+        Color.dynamicBackgroundColor(for: colorScheme)
+            .edgesIgnoringSafeArea(.all)
+    }
+    
+    /// The main content group containing the list of cryptocurrencies or appropriate messages.
+    private var contentGroup: some View {
+        Group {
+            if viewModel.isLoading && filteredCryptos.isEmpty {
+                ProgressView()
+            } else if !filteredCryptos.isEmpty {
+                cryptoScrollView
+            } else {
+                Text("Nothing To Show!")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+        }
+    }
+    
+    /// Scroll view containing the list of cryptocurrencies.
     private var cryptoScrollView: some View {
         ScrollView {
             LazyVStack(spacing: 0) {
                 ForEach(filteredCryptos, id: \.id) { crypto in
-                    NavigationLink(destination: CryptoDetailView(cryptos: cryptos, index: cryptos.lastIndex(of: crypto) ?? 0)) {
-                        CryptoRow(crypto: crypto)
-                            .transition(.slide)
-                            .animation(.easeInOut, value: filteredCryptos)
+                    VStack {
+                        cryptoRow(for: crypto)
+                        Divider().padding(.horizontal)
                     }
-                    .buttonStyle(PlainButtonStyle())
-                    Divider()
-                        .padding(.horizontal)
                 }
-                Spacer()
-                    .frame(height: 50)  // Add spacer at the bottom
+                Spacer().frame(height: 50)  // Add spacer at the bottom
             }
-            
-        }.refreshable {
+        }
+        .refreshable {
             viewModel.loadMarketData(isRefresh: true)
         }
     }
-
     
-}
-
-
-
-struct TrackView_Previews: PreviewProvider {
-    static var previews: some View {
-        TrackView(viewModel: TrackViewModel())
+    /// View for a single cryptocurrency row.
+    /// - Parameter crypto: The cryptocurrency data to display.
+    /// - Returns: A view representing a single row.
+    private func cryptoRow(for crypto: Cryptocurrency) -> some View {
+        NavigationLink(destination: CryptoDetailView(cryptos: cryptos, index: cryptos.lastIndex(of: crypto) ?? 0)) {
+            CryptoRow(crypto: crypto)
+                .transition(.slide)
+                .animation(.easeInOut, value: filteredCryptos)
+        }
+        .buttonStyle(PlainButtonStyle())
+    
+    }
+    
+    // MARK: - Previews
+    struct TrackView_Previews: PreviewProvider {
+        static var previews: some View {
+            TrackView(viewModel: TrackViewModel())
+        }
     }
 }
-
